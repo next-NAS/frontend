5c4ef39e6e1273cbbc67d2be9adc5f2bddcdefc1
Sword-holder <iamthexzy@gmail.com>
2019-11-29 22:15:27 +0800

    增加了任务选择时hover的悬浮效果

--- src/components/createTask/TaskSelector.vue
+++ src/components/createTask/TaskSelector.vue
@@ -25,9 +25,12 @@ export default {
 <style scoped>
 
 .available {
-  color:rgb(77, 165, 236);
+  color:rgb(79, 166, 238);
+  transition: 0.3s;
 }
 .available:hover {
+  color:rgb(16, 138, 238);
+  margin-top: -6px;
   cursor: pointer;
 }
 .disable {
